a7778ca899314e5b2d8e11f80a409b4d24fc1dff
dimansipl <dmitry.osipov2000@mail.ru>
2025-09-25 15:34:31 +0300

    Добавляем астероиды и кристаллы

---
+++ classes/object.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+
+class FallingObject:
+    def __init__(self, x, y, width, height, color, speed):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.speed = speed
+
+    def update(self):
+        self.rect.y += self.speed
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+
+    def is_off_screen(self, screen_height):
+        return self.rect.top > screen_height
+
+class Crystal(FallingObject):
+    def __init__(self, screen_width):
+        x = random.randint(20, screen_width - 40)
+        super().__init__(x, -30, 20, 20, (0, 255, 0), 5)  # Зеленый
+
+class Asteroid(FallingObject):
+    def __init__(self, screen_width):
+        x = random.randint(20, screen_width - 50)
+        super().__init__(x, -30, 30, 30, (255, 0, 0), 5)  # Красный
--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
+import random
 from classes.player import Player
+from classes.object import Crystal, Asteroid
 
 pygame.init()
 
@@ -12,20 +14,48 @@ clock = pygame.time.Clock()
 
 player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 50, SCREEN_WIDTH)
 
+crystals = []
+asteroids = []
+spawn_timer = 0
+
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_a]:
         player.move(-1)
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_d]:
         player.move(1)
     
+    spawn_timer += 1
+    if spawn_timer  >= 30:
+        spawn_timer = 0
+        if random.random() < 0.6:
+            crystals.append(Crystal(SCREEN_WIDTH))
+        else: 
+            asteroids.append(Asteroid(SCREEN_WIDTH))
+
+    for crystal in crystals[:]:
+        crystal.update()
+        if crystal.is_off_screen(SCREEN_HEIGHT):
+            crystals.remove(crystal)
+    
+    for asteroid in asteroids[:]:
+        asteroid.update()
+        if asteroid.is_off_screen(SCREEN_HEIGHT):
+            asteroids.remove(asteroid)
+
     screen.fill((0, 0, 0)) 
     player.draw(screen)
+
+    for crystal in crystals:
+        crystal.draw(screen)
+    for asteroid in asteroids:
+        asteroid.draw(screen)
+
     pygame.display.flip()
     clock.tick(60)
 
