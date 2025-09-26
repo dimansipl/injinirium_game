86b695bc2fabaeb88ac02157de258cbbfadbafc6
dimansipl <dmitry.osipov2000@mail.ru>
2025-09-26 18:13:50 +0300

    ошибка...

--- database/database_manager.py
+++ database/database_manager.py
@@ -1,24 +1,27 @@
 import sqlite3
 
+
 class GameDatabase:
     def __init__(self):
         self.db_file = "database/records.db"
         self.create_table()
-    
+
     def create_table(self):
         conn = sqlite3.connect(self.db_file)
         cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS winners (name TEXT, score INTEGER, time REAL)")        
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS winners (name TEXT, score INTEGER, time REAL)"
+        )
         conn.commit()
         conn.close()
-    
+
     def save_winner(self, name, score, time):
         conn = sqlite3.connect(self.db_file)
         cur = conn.cursor()
         cur.execute("INSERT INTO winners VALUES (?, ?, ?)", (name, score, time))
         conn.commit()
         conn.close()
-    
+
     def get_top_winners(self):
         conn = sqlite3.connect(self.db_file)
         cur = conn.cursor()
